81a428c217eba2fdf0b8c4bfbd67fa2a785901ac
Gustavo Saiani <gustavo@poe.ma>
2021-10-25 15:31:37 -0300

    Autogrow

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AceEditor from "react-ace";
 import { Container } from "./App.styled";
 
 import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
 
 function onChange(newValue) {
   console.log("change", newValue);
@@ -13,10 +13,21 @@ function App() {
     <Container>
       <AceEditor
         mode="javascript"
-        theme="github"
+        theme="monokai"
         onChange={onChange}
         name="UNIQUE_ID_OF_DIV"
         editorProps={{ $blockScrolling: true }}
+        fontSize={18}
+        setOptions={{
+          enableBasicAutocomplete: true,
+          minLines: 1,
+          maxLines: 4,
+          showLineNumbers: false
+        }}
+        showGutter={false}
+        style={{
+          width: 1000
+        }}
       />
     </Container>
   );
